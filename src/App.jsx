6fabf95588a4661e4b6b80e9f3054afcfe275e32
KSHITIJ JAISWAL <kshitij.chetan030174@gmail.com>
2025-11-19 16:01:15 +0530

    pages and componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Home from './pages/Home.jsx'
 
 function App() {
 
 
   return (
     <>
-     <h1 class="text-6xl font-bold underline">
+     <h1 className="text-6xl font-bold underline">
     Hello world!
   </h1>
     </>
